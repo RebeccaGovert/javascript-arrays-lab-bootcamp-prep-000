7c632f0351d573e31c653f5e7e804db5a918e0fd
Rebecca Govert <becci.govert@gmail.com>
2018-04-25 15:28:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ function destructivelyRemoveLastKitten() { //mutation remove lastKittenName off 
 
 function destructivelyRemoveFirstKitten() { //mutation remove lastKittenName off Beginning of kittens
   return kittens.shift();
-} // kittens now equals 
+} // kittens now equals ["Otis", "Garfield"] 
 
 function appendKitten(name) { //add newKittenName to End and give newKittensArray (no mutation)
   var newKittens = [...kittens, name]
   return newKittens;
-}
+} //newkittens returns []
 
 function prependKitten(name) { //add newKittenName to Beginning and give newKittensArray
   var newKittens = [name, ...kittens]
